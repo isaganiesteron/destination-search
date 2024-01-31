3226f33ec4739d1e0a4567042b0298b30cca16f4
Isagani Esteron <isaganiesteron@ymail.com>
2024-01-31 20:35:03 +0800

    added search result item template

--- app/test-interface/page.tsx
+++ app/test-interface/page.tsx
@@ -17,7 +17,7 @@ const page = () => {
 
 				<div>
 					<p className="font-bold text-md">City:</p>
-					<select className="border border-black rounded-md w-full" name="cities" id="cities" disabled>
+					<select className="border border-black rounded-md w-full " name="cities" id="cities" disabled>
 						<option value="1">1</option>
 						<option value="2">2</option>
 						<option value="3">3</option>
@@ -27,8 +27,21 @@ const page = () => {
 
 				<div>
 					<p className="font-bold text-md">Top 10 Hotels:</p>
-					<div className="border border-black rounded-md h-96 p-2">
+					<div className="border border-black rounded-md h-96 p-2 flex flex-col">
 						<p className="text-sm">[status] Please Choose Country... </p>
+						<div className="border border-black rounded-md flex flex-col w-full mt-2">
+							<div className="grid grid-cols-6 gap-1">
+								<div className="p-2 flex items-center align-middle">
+									<p>1. Image</p>
+								</div>
+								<div className="p-2 col-span-5 items-center">
+									<div className="flex flex-col">
+										<h1>Title</h1>
+										<p>description</p>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 
