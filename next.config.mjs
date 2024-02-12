ff18bc84d6ab6871ad47518d61f9d2e66bea2f9f
Isagani Esteron <isaganiesteron@ymail.com>
2024-02-12 09:40:39 +0800

    added basepath and removed github workflow for now

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	basePath: "/destination-search",
+	output: "export",
 	images: {
 		domains: ["booking.com", "q-xx.bstatic.com"],
 	},
