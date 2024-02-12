1a7ac7d4233a559e36a4f47a5dfb27a7a21a35f2
Isagani Esteron <isaganiesteron@ymail.com>
2024-02-12 10:11:27 +0800

    added basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	// basePath: "/destination-search",
+	basePath: "/destination-search",
 	// output: "export",
 	images: {
 		domains: ["booking.com", "q-xx.bstatic.com"],
