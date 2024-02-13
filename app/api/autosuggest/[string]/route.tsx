a3547edd16c2652217aa625ca4eb009bdee61e17
Isagani Esteron <isaganiesteron@ymail.com>
2024-02-13 10:02:24 +0800

    endpoint for autosuggest added

---
+++ app/api/autosuggest/[string]/route.tsx
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server"
+export async function GET(request: Request, params: any) {
+	const { string } = params.params
+
+	try {
+		const response = await fetch(`https://accommodations.booking.com/autocomplete.json`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ query: string, language: "en-us", size: 5 }),
+		})
+		if (response.status === 200) {
+			const data = await response.json()
+			return NextResponse.json(data, { status: 200 })
+		} else {
+			return NextResponse.json(response, { status: 500 })
+		}
+	} catch (error) {
+		console.log(error)
+		return NextResponse.json({ error }, { status: 500 })
+	}
+}
