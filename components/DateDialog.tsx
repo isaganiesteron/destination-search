5030874b8d141b629a17ae9511242e7fa6aecf37
Isagani Esteron <isaganiesteron@ymail.com>
2024-03-02 10:57:54 +0800

    DateDialog now has some functionality

--- components/DateDialog.tsx
+++ components/DateDialog.tsx
@@ -6,23 +6,29 @@ type DateDialogProps = {
   closeDialog: Function;
 };
 
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
 const DateDialog = ({ setCurrentDates, closeDialog }: DateDialogProps) => {
   const [dateOptions, setDateOptions] = useState<object[]>();
+  const [currentDuration, setCurrentDuration] = useState<string>("day");
+  const [currentMonthYear, setCurrentMonthYear] = useState<string>(
+    `${moment().format("MMM")}${moment().format("YYYY")}`
+  );
+
   useEffect(() => {
-    const months = [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ];
     let currentMonthIndex = moment().month(); // get the current month as a zero-based index
     setDateOptions([
       ...months.slice(currentMonthIndex).map((x) => {
@@ -33,6 +39,19 @@ const DateDialog = ({ setCurrentDates, closeDialog }: DateDialogProps) => {
       }),
     ]);
   }, []);
+
+  useEffect(() => {
+    if (currentDuration !== "" && currentMonthYear !== "") {
+      console.log("Duration and month/year selected");
+      console.log(currentDuration, currentMonthYear);
+      if (currentMonthYear === "") {
+        // make month and year current
+      }
+    }
+
+    // setCurrentDates({ checkin: "2022-01-01", checkout: "2022-01-02" });
+  }, [currentDuration, currentMonthYear]);
+
   return (
     <div className="p-4 w-full border-2 border-black flex flex-col rounded-md gap-3">
       <div className="flex flex-col">
@@ -40,31 +59,37 @@ const DateDialog = ({ setCurrentDates, closeDialog }: DateDialogProps) => {
         <div className="flex flex-row">
           <div className="flex flex-row gap-2 px-4 p-1">
             <input
+              checked={currentDuration === "day"}
               type="radio"
               name="duration"
-              onClick={() => {
-                console.log("Chose A");
-              }}
+              onClick={() => setCurrentDuration("day")}
+            />
+            <p>A day</p>
+          </div>
+          <div className="flex flex-row gap-2 px-4 p-1">
+            <input
+              checked={currentDuration === "weekend"}
+              type="radio"
+              name="duration"
+              onClick={() => setCurrentDuration("weekend")}
             />
             <p>A weekend</p>
           </div>
           <div className="flex flex-row gap-2 px-4 p-1">
             <input
+              checked={currentDuration === "week"}
               type="radio"
               name="duration"
-              onClick={() => {
-                console.log("Chose B");
-              }}
+              onClick={() => setCurrentDuration("week")}
             />
             <p>A week</p>
           </div>
           <div className="flex flex-row gap-2 px-4 p-1">
             <input
+              checked={currentDuration === "month"}
               type="radio"
               name="duration"
-              onClick={() => {
-                console.log("Chose C");
-              }}
+              onClick={() => setCurrentDuration("month")}
             />
             <p>A month</p>
           </div>
@@ -73,17 +98,26 @@ const DateDialog = ({ setCurrentDates, closeDialog }: DateDialogProps) => {
         <div className="flex flex-row flex-wrap">
           {dateOptions &&
             dateOptions.map((month, i) => {
+              const curSelected =
+                currentMonthYear ===
+                `${month["month" as keyof typeof month]}${
+                  month["year" as keyof typeof month]
+                }`;
               return (
                 <button
                   key={i}
-                  className="p-4 m-2 border border-gray-500 rounded-md hover:bg-gray-200"
-                  onClick={() => {
-                    console.log(
-                      `Chose ${month["month" as keyof typeof month]} ${
+                  className={`p-4 m-2 border rounded-md ${
+                    curSelected
+                      ? "border-blue-900 bg-blue-100"
+                      : "border-gray-500 hover:bg-gray-200"
+                  } `}
+                  onClick={() =>
+                    setCurrentMonthYear(
+                      `${month["month" as keyof typeof month]}${
                         month["year" as keyof typeof month]
                       }`
-                    );
-                  }}
+                    )
+                  }
                 >
                   <p className="font-bold">
                     {month["month" as keyof typeof month]}
