c3df3a7d90c5d52abe71060b69e1c322b6e33080
Isagani Esteron <isaganiesteron@ymail.com>
2024-03-27 22:09:16 +0800

    select and unselect all button

--- components/Districts.tsx
+++ components/Districts.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Districts = ({
   currentDistricts,
@@ -9,31 +9,50 @@ const Districts = ({
   selectedDistricts: number[];
   setSelectedDistricts: Function;
 }) => {
+  const [allIsChecked, setAllIsChecked] = useState<boolean>(false);
   return (
-    <div className="grid grid-cols-5 gap-1 ">
-      {currentDistricts.map((district, i) => {
-        const districtName = district["name" as keyof typeof district];
-        const districtId = district["id" as keyof typeof district];
-        return (
-          <div key={i} className="flex flex-row items-center">
-            <input
-              type="checkbox"
-              checked={selectedDistricts.includes(districtId)}
-              onChange={(e) => {
-                if (e.target.checked) {
-                  setSelectedDistricts([...selectedDistricts, districtId]);
-                } else {
-                  setSelectedDistricts(
-                    selectedDistricts.filter((id) => id !== districtId)
-                  );
-                }
-              }}
-            />
-            <p className="text-sm ml-1">{districtName}</p>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className="grid grid-cols-5 gap-1 ">
+        {currentDistricts.map((district, i) => {
+          const districtName = district["name" as keyof typeof district];
+          const districtId = district["id" as keyof typeof district];
+          return (
+            <div key={i} className="flex flex-row items-center">
+              <input
+                type="checkbox"
+                className="district-checkbox"
+                checked={selectedDistricts.includes(districtId)}
+                onChange={(e) => {
+                  if (e.target.checked) {
+                    setSelectedDistricts([...selectedDistricts, districtId]);
+                  } else {
+                    setSelectedDistricts(
+                      selectedDistricts.filter((id) => id !== districtId)
+                    );
+                  }
+                }}
+              />
+              <p className="text-sm ml-1">{districtName}</p>
+            </div>
+          );
+        })}
+      </div>
+
+      <button
+        className="justify-center mt-2 p-1 text-xs text-blue-800 font-semibold underline hover:text-blue-950 hover:font-extrabold"
+        onClick={() => {
+          const currentAllSelect = allIsChecked
+            ? []
+            : currentDistricts.map(
+                (district) => district["id" as keyof typeof district]
+              );
+          setSelectedDistricts(currentAllSelect);
+          setAllIsChecked(!allIsChecked);
+        }}
+      >
+        {allIsChecked ? "Unselect All" : "Select All"}
+      </button>
+    </>
   );
 };
 
