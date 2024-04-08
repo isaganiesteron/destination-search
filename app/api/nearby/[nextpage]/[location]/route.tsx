6839e629d584f00c45c010f1472b8b071dafc837
Isagani Esteron <isaganiesteron@ymail.com>
2024-04-08 16:32:35 +0800

    added 3 new google maps apis

---
+++ app/api/nearby/[nextpage]/[location]/route.tsx
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request, params: any) {
+  const { nextpage, location } = params.params;
+  try {
+    const parameter =
+      nextpage !== "null"
+        ? `pagetoken=${nextpage}`
+        : `location=${location}&radius=100000&type=lodging`;
+    const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?${parameter}&key=${process.env.GOOGLE_MAPS_KEY}`;
+    console.log("Fetching: " + url);
+    const response = await fetch(url);
+    const data = await response.json();
+
+    // /** FOR DEV PURPOSES */
+    // const mappedData = {
+    // 	next_page_token: data.next_page_token,
+    // 	next_page_token_link: `http://localhost:3000/api/nearby/${data.next_page_token}/null`,
+    // 	count: data.results.length,
+    // 	results: data.results.map((result: any) => {
+    // 		return {
+    // 			name: result.name,
+    // 			types: result.types,
+    // 		}
+    // 	}),
+    // }
+    // return NextResponse.json(mappedData)
+    // /** FOR DEV PURPOSES */
+
+    return NextResponse.json(data);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
